64998e0743bb21162589b16eb499c3e45c032e3d
marick <mmaarriicckk@gmail.com>
2020-04-23 18:16:33 +0800

    add

--- includes/db.php
+++ includes/db.php
@@ -17,4 +17,19 @@ if(mysqli_connect_errno($connection)){
 
     echo "connect error";
 }
+function send_post($url, $post_data){
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+          'method' => 'POST',
+          'header' => 'Content-type:application/x-www-form-urlencoded',
+          'content' => $postdata,
+          'timeout' => 15 * 60 // 超時時間（單位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+}
 ?>
+
