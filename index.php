a852093460673eaa171aa1581a5281ae1357dfae
marick <mmaarriicckk@gmail.com>
2020-05-05 17:56:20 +0800

    add

--- index.php
+++ index.php
@@ -12,69 +12,45 @@ include "./includes/navigation.php";
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
+                <?php 
+                    $query = "SELECT * FROM posts";
+                    $num=0;
+                    $select_all_posts_query=mysqli_query($connection,$query);
+                    while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                        $post_title=$row["post_title"];
+                        $post_author=$row["post_author"];
+                        $post_date=$row["post_date"];
+                        $post_image=$row["post_image"];
+                        $post_content=$row["post_content"];
+                        $post_tags=$row["post_tags"];
+                        echo "<h2><a href='#'>{$post_title}</a></h2>";
+                        echo "<p class='lead'>by {$post_author}</p>";
+                        echo "<p><span class='glyphicon glyphicon-time'></span> Posted on {$post_date}</p>";
+                        echo "<hr> <img class='img-responsive' src='http://placehold.it/900x300' alt=''> <hr>";
+                        echo "<p>{$post_content}</p> <hr>";
+                        $num +=1;
+                    }
+                    $string = json_encode($row, JSON_UNESCAPED_UNICODE);
+                    $length = count($string);
+                    /*$length = strlen($string); */
+                    echo "{$length}";
+                    /* 當頁數 */
+                    if($length > 0 ){
+                        echo "<ul class='pager'>
+                                <li class='previous'>
+                                    <a href='#'>&larr; Older</a>
+                                </li>
+                                <li class='next'>
+                                    <a href='#'>Newer &rarr;</a>
+                                </li>
+                            </ul>";
+                    }
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Second Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Third Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
+                ?>
 
             </div>
 
